273a58cedf456fb60966816c5337290cf0b69227
Tani Umetsu <taniu@hawaii.edu>
2023-10-10 13:30:44 -1000

    Parts 1-7 completed

--- Hackwich Six/Hackwich Six/ViewController.swift
+++ Hackwich Six/Hackwich Six/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    var myFriendsArray = ["Erika", "Jacob", "Teva"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriendsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = myFriendsArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
+
+
 
